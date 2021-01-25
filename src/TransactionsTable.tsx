2d99d870419ac5fed9b22acb8f34c5b94ee8f920
Dilan <dilanustek@gmail.com>
2021-01-24 19:12:18 -0800

    Show total balance

--- src/TransactionsTable.tsx
+++ src/TransactionsTable.tsx
@@ -6,6 +6,16 @@ interface Props {
   allTransactions: Transaction[];
 }
 class TransactionsTable extends Component<Props, {}> {
+  /*   Compute balance when table is rendered so that if there are changes
+    to transactions, the balance can change dynamically */
+  computeBalance() {
+    let balance = 0;
+    this.props.allTransactions.forEach((tr) => {
+      balance += parseFloat(tr.amount);
+    });
+    return balance;
+  }
+
   render() {
     return (
       <table>
@@ -14,7 +24,7 @@ class TransactionsTable extends Component<Props, {}> {
             <th>Date</th>
             <th>Company</th>
             <th>Ledger</th>
-            <th>Amount</th>
+            <th>{this.computeBalance()}</th>
           </tr>
         </thead>
         <tbody>
